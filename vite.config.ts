8af7f9714200eab8fed672e490363864948142f9
syp <ZIxiao2198839712@outlook.com>
2024-06-22 10:57:08 +0800

    修改配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({mode}) => {
     // 根据当前工作目录中的 `mode` 加载 .env 文件
     // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
     const env = loadEnv(mode, process.cwd(), '')
@@ -18,18 +18,17 @@ export default defineConfig(({command, mode}) => {
         server: {
             host: '0.0.0.0',
             port: 8080,
-            // hmr: {
-            //     protocol: 'ws',
-            //     host: url.IP,
-            // },
             proxy: {
                 '/api': {
                     target: env.VITE_LOGIN_API,
                     changeOrigin: true,
                     ws: true,
-                    rewrite: path => path.replace(/^\/api/, ''),
+                    rewrite: (path: string) => path.replace(/^\/api/, '')
                 }
             },
+        },
+        build: {
+            outDir: 'static'
         },
     }
 });
