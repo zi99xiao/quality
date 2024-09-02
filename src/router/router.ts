3f727f511d13d578cd0e500e61dde9f942a0f19c
syp <ZIxiao2198839712@outlook.com>
2024-09-02 09:47:24 +0800

    路由配置更改

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,12 +1,17 @@
-import {createRouter, createWebHistory, Router} from 'vue-router'
+import {createRouter, createWebHashHistory, Router} from 'vue-router'
 import {routes} from "./route";
 import {getCookie} from "../utils/cookie";
 import {NStart, NClose} from "../utils/nprogress";
 
 
 const router: Router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
+    // 路由配置
     routes,
+    // 是否严格匹配路由
+    strict: true,
+    // 路由跳转完成后，页面滚动行为
+    scrollBehavior: () => ({ left: 0, top: 0 }),
 })
 
 router.beforeEach(async (to, from) => {
